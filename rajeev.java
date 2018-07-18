34f7d766dbd87b2c09eb58ebab03dfa5a1e7dc69
“[Rajeev Ranjan]" <[email.rajeevranjan@gmail.com]>
2018-07-18 14:46:55 +0530

    its my code

---
+++ rajeev.java
@@ -0,0 +1,27 @@
+package com.practice;
+
+public class Duplicate0
+{
+	public static void main(String[] args)
+	{
+	
+		for (int i=0; i<arr.length; i++)
+		{
+			if (arr[i]>0)
+			{
+				res[k]=arr[i];
+				k++;
+			}
+		}
+		for (int i=0; i<arr.length; i++)
+		{
+			System.out.print(arr[i]);
+		}
+		System.out.println();
+		
+		for (int i=0; i<count; i++)
+		{
+			System.out.print(res[i]);
+		}
+	}
+}
